5f3d7af624dd9ab9b5cb004b100b1884df81db28
Ssachin2003an <sachidananda339@gmail.com>
2026-05-22 21:49:14 +0530

    fix

--- Backend/controllers/usersControllers.js
+++ Backend/controllers/usersControllers.js
@@ -146,10 +146,10 @@ const userLogin = async (req, res) => {
     }
 
     return res.status(200).json({
-      id: user.id,
-      name: user.user_name,
+      _id: user._id,
+      user_name: user.user_name,
       email: user.email,
-      token: generateUsersToken(user.id, user.email),
+      token: generateUsersToken(user._id, user.email),
     });
   } catch (error) {
     console.error("Login error:", error.message);
